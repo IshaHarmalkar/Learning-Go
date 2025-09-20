3cd7708157f8c004132fc8d8995a64b4f71c1ee5
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-20 18:11:37 +0530

    File read by taking file name as arg

--- interface-assignment-2/main.go
+++ interface-assignment-2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -10,14 +11,20 @@ func main() {
 
 	//create a program that reads the contents of a text file
 	//  then prints its content to the terminal
-
-	f, err := os.OpenFile("myfile.txt", os.O_RDWR|os.O_CREATE, 0644)
+    fileName := os.Args[1]
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
 
 	if err!= nil {
 		log.Fatal(err)
 	} else{
 		fmt.Println("File reading possible", f)
+		
 	}
+
+
+	io.Copy(os.Stdout, f)
+
+
 
 
 
