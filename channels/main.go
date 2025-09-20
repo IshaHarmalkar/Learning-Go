59b9d001c2c5c2acef9006d92a427b516fd0d522
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-20 21:29:19 +0530

    Channels

--- channels/main.go
+++ channels/main.go
@@ -15,20 +15,30 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 
 	}
 
+	fmt.Println(<- c)
+
+	//we only use the go key word in front of a fn call.
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
+
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+
+	c <- "Yup it's up"
 }
