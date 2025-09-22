3b695435f8594f6b17ec1abe126d5dd0cc9e8243
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-22 19:27:50 +0530

    Access Points

--- api/endpoints.go
+++ api/endpoints.go
@@ -10,17 +10,23 @@ import (
 
 
 
+type Pagination struct {
+	Page int `json:"page"`
+	PerPage int `json:"perPage"`
+}
+
+
+type Filters struct {
+	Search *string `json:"search"`
+}
 
 
 type Payload struct {
 	Pagination Pagination `json:"pagination"`
+	Filters    *Filters   `json:"filters,omitempty"`
 }
 
 
-type Pagination struct {
-	Page    int `json:"page"`
-	PerPage int `json:"perPage"`
-}
 
 
 func getSites(){
@@ -51,4 +57,43 @@ func getSites(){
 	handleResponse(resp)
 
 }
+
+
+
+
+func getAccessPoint(){
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("JWT_TOKEN")
+
+	getSitesUrl := os.Getenv("POST_ACCESS_POINTS")
+
+	var search *string = nil
+
+	payload := Payload{
+	Pagination: Pagination{
+		Page:    1,
+		PerPage: 40,
+	},
+	Filters: &Filters{
+		Search: search,
+	},
+}
+
+
+
+	resp, err := makeRequest("POST", getSitesUrl, token, payload)
+	if err != nil{
+		fmt.Println("error in making request")
+
+	}
+	handleResponse(resp)
+
+}
+
 
--- api/main.go
+++ api/main.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fmt.Println("Get Sites")
 	getSites()
+
+
+	fmt.Println("Get Access Points")
+	getAccessPoint()
 
 }
