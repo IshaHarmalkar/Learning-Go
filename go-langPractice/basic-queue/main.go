d838f2ec5b45f384f3eb80f93bb38cfdb01ef1f0
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-22 11:17:47 +0530

    Blog sol using structs

--- go-langPractice/basic-queue/main.go
+++ go-langPractice/basic-queue/main.go
@@ -1,51 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
-
-	var queue = make([]int, 0)
-
-	queue = enqueue(queue, 10)
-	fmt.Println("After pushing 10 ", queue)
-
-
-	queue = enqueue(queue, 20)
-	fmt.Println("After pushing 20 ", queue)
-
-
-	queue = enqueue(queue, 30)
-	fmt.Println("After pushing 20 ", queue)
-
-
-	ele, queue := deque(queue)
-	fmt.Println("Queue after removing", ele,  " :", queue)
-
-	queue = enqueue(queue, 40)
-	fmt.Println("After pushing 40 ", queue)
-
-}
-
-func enqueue(queue []int, element int) []int {
-
-	queue = append(queue, element) //append to q
-	fmt.Println("Enqueued:", element)
-
-	return queue
-
+type Queue struct {
+    Elements []int
+    Size     int
 }
 
 
-func deque(queue []int) (int, []int) {
 
-	element := queue[0]
+func main(){
 
-	
-	if len(queue) == 1 {
-		var tmp = []int{}
-		return element, tmp
+	q := Queue{Size: 3}  //empty q with struct, specified fields
+
+	fmt.Println(q.Elements)
+
+	q.Enqueue(1)
+	fmt.Println(q.Elements)
+
+	q.Enqueue(2)
+	fmt.Println(q.Elements)
+
+	q.Enqueue(3)
+	fmt.Println(q.Elements)
+
+
+	q.Enqueue(4)
+	fmt.Print(q.Elements)
+
+
+	q.Enqueue(5)
+	fmt.Println(q.Elements)
+
+
+	elem := q.Dequeue()
+	fmt.Print(elem)
+
+	fmt.Println(q.Elements)
+
+	q.Enqueue(9)
+	fmt.Println(q.Elements)
+
+
+	elem = q.Dequeue()
+	fmt.Print(elem)
+
+	fmt.Print(q.Elements)
+
+
+	fmt.Println("Queue empty", q.IsEmpty())
+
+
+		if !q.IsEmpty(){
+			
+			fmt.Println("Quee has elements")
+		}
+
+
+
+
+}
+
+
+func (q *Queue) Enqueue(elem int) {
+    if q.GetLength() == q.Size {
+        fmt.Println("Overflow")
+        return
+    }
+    q.Elements = append(q.Elements, elem)
+}
+
+
+func (q *Queue) Dequeue() int {
+	if q.IsEmpty(){
+		fmt.Println("UnderFlow")
+		return 0
+	}
+	element := q.Elements[0]
+	if q.GetLength() == 1 {
+		q.Elements = nil
+		return element
 	}
 
-	return element, queue[1:]   //slice off the element once it is dequeued
+	q.Elements = q.Elements[1:]
+	return element // Slice off the element once it's dequeued
 
+
+}
+
+func (q *Queue) GetLength() int {
+
+	return len(q.Elements)
+}
+
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.Elements) == 0
+}
+
+
+
+func (q *Queue)Peek() (int, error){
+	if q.IsEmpty(){
+		return 0, errors.New("empty queue")
+	}
+
+	return q.Elements[0], nil
 }
