1724e0f4b79c71049899f72fa2af4bacc114f666
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-22 11:46:14 +0530

    Blog sol linked list. Iterating over list

--- go-langPractice/basic-queue/main.go
+++ go-langPractice/basic-queue/main.go
@@ -1,112 +1,32 @@
 package main
 
 import (
-	"errors"
+	"container/list"
 	"fmt"
 )
 
-type Queue struct {
-    Elements []int
-    Size     int
-}
+func main() {
+
+	//new linked list
+
+	q := list.New()
+
+	//simply append to enque
+	q.PushBack(10)
+	q.PushBack(20)
+	q.PushBack(30)
+	
+     for e:= q.Front(); e != nil; e = e.Next(){
+		fmt.Println(e.Value)
+	 }
+
+	//deque
+
+	front := q.Front()
+	fmt.Println(front.Value)
+
+	q.Remove(front)
 
 
 
-func main(){
-
-	q := Queue{Size: 3}  //empty q with struct, specified fields
-
-	fmt.Println(q.Elements)
-
-	q.Enqueue(1)
-	fmt.Println(q.Elements)
-
-	q.Enqueue(2)
-	fmt.Println(q.Elements)
-
-	q.Enqueue(3)
-	fmt.Println(q.Elements)
-
-
-	q.Enqueue(4)
-	fmt.Print(q.Elements)
-
-
-	q.Enqueue(5)
-	fmt.Println(q.Elements)
-
-
-	elem := q.Dequeue()
-	fmt.Print(elem)
-
-	fmt.Println(q.Elements)
-
-	q.Enqueue(9)
-	fmt.Println(q.Elements)
-
-
-	elem = q.Dequeue()
-	fmt.Print(elem)
-
-	fmt.Print(q.Elements)
-
-
-	fmt.Println("Queue empty", q.IsEmpty())
-
-
-		if !q.IsEmpty(){
-			
-			fmt.Println("Quee has elements")
-		}
-
-
-
-
-}
-
-
-func (q *Queue) Enqueue(elem int) {
-    if q.GetLength() == q.Size {
-        fmt.Println("Overflow")
-        return
-    }
-    q.Elements = append(q.Elements, elem)
-}
-
-
-func (q *Queue) Dequeue() int {
-	if q.IsEmpty(){
-		fmt.Println("UnderFlow")
-		return 0
-	}
-	element := q.Elements[0]
-	if q.GetLength() == 1 {
-		q.Elements = nil
-		return element
-	}
-
-	q.Elements = q.Elements[1:]
-	return element // Slice off the element once it's dequeued
-
-
-}
-
-func (q *Queue) GetLength() int {
-
-	return len(q.Elements)
-}
-
-
-func (q *Queue) IsEmpty() bool {
-	return len(q.Elements) == 0
-}
-
-
-
-func (q *Queue)Peek() (int, error){
-	if q.IsEmpty(){
-		return 0, errors.New("empty queue")
-	}
-
-	return q.Elements[0], nil
 }
