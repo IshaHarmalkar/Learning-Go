657e848bdea1fd9287e77fec4586eb7e0fc33f8d
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2025-09-25 12:21:56 +0530

    Refactored to fns

--- go-routines/buffered_channel/main.go
+++ go-routines/buffered_channel/main.go
@@ -5,24 +5,49 @@ import (
 	"time"
 )
 
-func main() {
-	orders := make(chan string, 3) //buffered chan with capacity 3
+func placeOrders(orders chan string, numOrders int){
+	for i := 1; i <= numOrders; i++{
+		order := fmt.Sprintf("Coffee order #%d", i)
+		orders <- order //blocks only if buffer is full
+		fmt.Println("Placed:", order)
+	}
 
-	go func() {
-		for i := 1; i <= 5; i++ {
-			order := fmt.Sprintf("Coffee order #%d", i)
-			orders <- order //only blocks if the buffer is full
-			fmt.Println("Placed: ", order)
-		}
+	close(orders) //signal no more orders
+}
 
-		close(orders)
-	}()
 
-	//Barissa processing orders
+func processOrders(orders chan string){
+
+	//receives unitl chan is closed
 	for order := range orders {
-		fmt.Printf("Preparing: 5s\n", order)
-		time.Sleep(2 * time.Second) //time taken to prepare the order
+
+		fmt.Printf("Preparin: %s\n", order)
+		time.Sleep(2 * time.Second)
+		fmt.Printf("Saved: %s\n", order)
+		time.Sleep(2 * time.Second) 
 		fmt.Printf("Served: %s\n", order)
+
 	}
 
 }
+
+func main() {
+	orders := make(chan string, 3) //buffered chan with capacity 3
+
+	//start producer in a go routine
+	go placeOrders(orders, 5)
+
+	processOrders(orders)
+}
+
+
+/* 
+	The barista will wait if the chan is empty.
+
+	Buffered channels
+	-Decouples producers and consumers
+	-Burts of traffic
+	-Improves throughput
+	-Great for worker pools
+	-Userful for asynchronous signals or events
+*/
